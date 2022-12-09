e49a87cf8a2066ef0d44c36010de27a208fea717
josiparapovic <josiparapovic@fpmoz.sum.ba>
2022-12-09 13:30:06 +0100

    peti

--- backend/src/components/AppLayout.vue
+++ backend/src/components/AppLayout.vue
@@ -2,11 +2,9 @@
     <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div class="w-full max-w-md space-y-8">
             <div>
-                <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
-                    {{title}}
-                </h2>
+                <h1>part</h1>
             </div>
-
+            <router-view></router-view>
         </div>
     </div>
 </template>
--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -18,7 +18,7 @@ const router = createRouter({
                   path: '/dashboard',
                   name: 'app.dashboard',
                   component: Dashboard
-              },
+              }
 
           ]
       },
