6766347fa1ad4500a9223eb446b86a60258138ae
josiparapovic <josiparapovic@fpmoz.sum.ba>
2022-12-20 13:43:48 +0100

    6

--- backend/src/router/index.js
+++ backend/src/router/index.js
@@ -4,6 +4,9 @@ import Dashboard from "@/views/Dashboard.vue";
 import Login from "@/views/Login.vue";
 import RequestPassword from "../views/RequestPassword.vue"
 import ResetPassword from "../views/ResetPassword.vue"
+import Products from "@/views/Products.vue";
+import store from "@/store";
+import NotFound from "@/views/NotFound.vue";
 
 
 const router = createRouter({
@@ -13,11 +16,19 @@ const router = createRouter({
           path:'/app',
           name:'app',
           component:AppLayout,
+          meta:{
+              requiresAuth: true
+          },
           children:[
               {
-                  path: '/dashboard',
+                  path: 'dashboard',
                   name: 'app.dashboard',
                   component: Dashboard
+              },
+              {
+                  path: 'products',
+                  name: 'app.products',
+                  component: Products
               }
 
           ]
@@ -25,20 +36,44 @@ const router = createRouter({
       {
           path: '/login',
           name: 'login',
+          meta:{
+              requiresGuest: true
+          },
           component: Login
       },
       {
-          path: '/request-password',
+          path: '/request-password/:token',
           name: 'requestPassword',
+          meta:{
+              requiresGuest: true
+          },
           component: RequestPassword
       },
       {
           path: '/reset-password',
           name: 'resetPassword',
+          meta:{
+              requiresGuest: true
+          },
           component: ResetPassword
+      },
+      {
+          path: '/:pathMatch(.*)',
+          name: 'notfound',
+          component: NotFound
       },
 
   ]
+})
+
+router.beforeEach((to,from,next)=>{
+    if(to.meta.requiresAuth && !store.state.user.token){
+        next({name:'login'})
+    }else if(to.meta.requiresGuest && store.state.user.token) {
+        next({name:'app.dashboard'})
+    } else{
+        next();
+    }
 })
 
 export default router
